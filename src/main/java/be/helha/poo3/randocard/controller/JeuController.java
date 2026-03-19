c99022be6cfddcd944b86ccf64575ac0c97f4758
Brillando <la249328@student.helha.be>
2026-03-19 11:04:11 +0100

    creation de la base de données automatique

---
+++ src/main/java/be/helha/poo3/randocard/controller/JeuController.java
@@ -0,0 +1,17 @@
+package be.helha.poo3.randocard.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JeuController {
+
+    /*@GetMapping("/verifierNombre/{:id}")
+    public boolean verifierNombre(@PathVariable("id") Integer id){
+
+
+
+    }*/
+}
