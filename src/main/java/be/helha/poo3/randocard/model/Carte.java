cbc233e640b4c306cba72539aa71d7c7a32ceb0f
aless <la248911@student.helha.be>
2026-03-19 11:41:56 +0100

    Création de la classe Carte

--- src/main/java/be/helha/poo3/randocard/model/Carte.java
+++ src/main/java/be/helha/poo3/randocard/model/Carte.java
@@ -1,8 +1,6 @@
 package be.helha.poo3.randocard.model;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.util.Random;
 
 @Data
@@ -20,5 +18,26 @@ public class Carte {
         Random rand = new Random();
         int nb = rand.nextInt(1,11);
         return nb;
+    }
+
+    public boolean verif(boolean reponseEntree){
+        // True = Bouton plus grand
+        // False = Bouton plus petit
+        boolean reponseCorrecte;
+
+        if (nbVisible < nbAdeviner){
+            reponseCorrecte = false;
+            // Si le nombre visible est plus petit que le nombre à deviner, la réponse est <
+        }
+        else {
+            reponseCorrecte = true;
+            // Si le nombre visible est plus grand que le nombre à deviner, la réponse est >
+        }
+
+        if (reponseEntree == reponseCorrecte){ // On vérifie la réponse entrée avec la réponse correcte
+            return true; // Bonne réponse ! On gagne des pièces et du score
+        } else {
+            return false; // Mauvaise réponse... On perd un coeur
+        }
     }
 }
