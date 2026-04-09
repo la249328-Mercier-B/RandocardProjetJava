d39d1ac87fec4d00750fcdb4d92edd8510e90e03
aless <la248911@student.helha.be>
2026-04-09 12:19:59 +0200

    Ajout de fonctions pour pouvoir acheter un item

--- src/main/java/be/helha/poo3/randocard/controller/JeuController.java
+++ src/main/java/be/helha/poo3/randocard/controller/JeuController.java
@@ -6,12 +6,16 @@ import be.helha.poo3.randocard.factory.ItemRepository;
 import be.helha.poo3.randocard.model.Item;
 import be.helha.poo3.randocard.model.Partie;
 import be.helha.poo3.randocard.model.Utilisateur;
+import be.helha.poo3.randocard.model.UtilisateurItem;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class JeuController {
@@ -67,5 +71,49 @@ public class JeuController {
     @GetMapping("/items")
     public List<Item> getItems() throws Exception {
         return itemRepository.findAll();
+    }
+
+    @GetMapping("/mesItems")
+    public List<UtilisateurItem> getMesItems(Authentication authentication) {
+        String pseudo = authentication.getName();
+        Utilisateur utilisateur = userDAO.findByPseudo(pseudo).orElseThrow();
+        return utilisateurItemDAO.findByUtilisateurId(utilisateur.getId());
+    }
+
+    @PostMapping("/acheter/{nomItem}")
+    public ResponseEntity<String> acheterItem(@PathVariable String nomItem,
+                              Authentication authentication) throws Exception {
+
+        String pseudo = authentication.getName();
+        Utilisateur utilisateur = userDAO.findByPseudo(pseudo).orElseThrow();
+
+        // Vérifier que l'item existe dans MongoDB
+        Optional<Item> itemOpt = itemRepository.findByNom(nomItem);
+        if (itemOpt.isEmpty()) {
+            return ResponseEntity.badRequest().body("Item inconnu : " + nomItem);
+        }
+        Item item = itemOpt.get();
+
+        // Vérifier que l'utilisateur a assez de pièces
+        if (utilisateur.getPieces() < item.getCout()) {
+            return ResponseEntity.badRequest().body("Pas assez de pièces !");
+        }
+
+        // Débiter les pièces
+        utilisateur.setPieces(utilisateur.getPieces() - item.getCout());
+        userDAO.save(utilisateur);
+
+        // Ajouter ou incrémenter la quantité
+        Optional<UtilisateurItem> existant = utilisateurItemDAO
+                .findByUtilisateurIdAndNomItem(utilisateur.getId(), nomItem);
+
+        if (existant.isPresent()) {
+            existant.get().setQuantite(existant.get().getQuantite() + 1);
+            utilisateurItemDAO.save(existant.get());
+        } else {
+            utilisateurItemDAO.save(new UtilisateurItem(0, utilisateur, nomItem, 1));
+        }
+
+        return ResponseEntity.ok("Item " + nomItem + " acheté !");
     }
 }
--- src/main/java/be/helha/poo3/randocard/factory/ItemRepository.java
+++ src/main/java/be/helha/poo3/randocard/factory/ItemRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ItemRepository {
@@ -19,12 +20,23 @@ public class ItemRepository {
     public List<Item> findAll() throws Exception {
         List<Item> items = new ArrayList<>();
         for (Document doc : collection.find()) {
-            Item item = ItemFactory.creerItem(doc.getString("nom"));
+            Item item = ItemFactory.creerItem(doc.getString("nom")); // Item vide
             item.setNom(doc.getString("nom"));
             item.setDescription(doc.getString("description"));
             item.setCout(doc.getInteger("cout"));
             items.add(item);
         }
         return items;
+    }
+
+    public Optional<Item> findByNom(String nom) throws Exception {
+        Document doc = collection.find(new Document("nom", nom)).first();
+        if (doc == null) {
+            return Optional.empty();
+        }
+        Item item = ItemFactory.creerItem(doc.getString("nom"));
+        item.setDescription(doc.getString("description"));
+        item.setCout(doc.getInteger("cout"));
+        return Optional.of(item);
     }
 }
